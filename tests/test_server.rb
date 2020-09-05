2ed29971d7d251c35e4570db46789418d152fe8a
Santiago <santo1996.29@gmail.com>
2020-09-05 14:14:56 -0400

    test: prediction on stop id endpoint

--- tests/test_server.rb
+++ tests/test_server.rb
@@ -22,4 +22,10 @@ class RootPathTest < Test::Unit::TestCase
 
     assert last_response.ok?
   end
+
+  def test_it_gets_stop_prediction_by_id
+    get PATH_PREFIX + 'prediction_by_id', params: {stop_id: '13385'}
+
+    assert last_response.ok?
+  end
 end
