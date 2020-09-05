1ae0d8874984c625f02127893f18a433edb6ff81
Santiago <santo1996.29@gmail.com>
2020-09-05 12:55:02 -0400

    feature: routes listing endpoint

--- bin/server.rb
+++ bin/server.rb
@@ -1,6 +1,9 @@
 require 'sinatra'
 require 'sinatra/json'
+require './lib/nextbus_consumer'
 
-get '/' do
-  json result: 'Is working.'
+CONSUMER = NextBusConsumer.new
+
+get '/api/v1/routes' do
+  json CONSUMER.routes_list
 end
