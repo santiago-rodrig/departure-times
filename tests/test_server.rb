25d0157e025fe2a5a59fbc0468d9be84ed0c293f
Santiago <santo1996.29@gmail.com>
2020-09-05 13:20:48 -0400

    test: route details endpoint

--- tests/test_server.rb
+++ tests/test_server.rb
@@ -9,8 +9,16 @@ class RootPathTest < Test::Unit::TestCase
     Sinatra::Application
   end
 
+  PATH_PREFIX = '/api/v1/'
+
   def test_it_gets_routes_list
-    get '/api/v1/routes'
+    get PATH_PREFIX + 'routes'
+
+    assert last_response.ok?
+  end
+
+  def test_it_gets_route_details
+    get PATH_PREFIX + 'route_details', params: {route_tag: 'MBUS'}
 
     assert last_response.ok?
   end
